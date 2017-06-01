7c1ee7e93120ae4264305a4f4f303c2057cae836
Aluno <aluno@cesed.local>
2017-06-01 19:33:13 -0300

    Primeiro commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,17 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', 'firebase', '$firebaseArray',
+function($scope, firebase, $firebaseArray) {
+  var ref = firebase.database().ref().child("messages");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
 
 }]);
